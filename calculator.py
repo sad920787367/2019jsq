9bd4a07affee1154cfb30f587ed87d9914a60d0e
sad920787367 <48972262+sad920787367@users.noreply.github.com>
2019-03-27 23:44:51 +0800

    更新版本
    取消了一些无用的功能

--- calculator.py
+++ calculator.py
@@ -87,6 +87,8 @@
 # root.mainloop()
 
 
+
+
 # from tkinter import *
 # from math import *
 # reset=True
@@ -113,6 +115,9 @@
 #     label1['text']=s+num
 #   except:
 #     label1['text']="ERROR"
+
+
+
 
 # #主窗口
 # root=Tk()
@@ -158,6 +163,10 @@
 # b.bind("<Button-1>",buttonCallBack)
 # root.mainloop()
 
+
+
+
+
 from tkinter import *
 from math import *
 reset=True
@@ -172,27 +181,31 @@ def buttonCallBack(event):
       return
     if num in "=":
       label2['text']=label1['text']
-      label1['text']=str(eval(label2['text']))
+      label1['text']=str(eval(label2['text']))[0:12]
+      reset=True
       return
     if num == "←":
       label1['text']=label1['text'][0:-1]
       return
-    s=label1['text'] 
-    if s=='0' or reset==True:
+    s=label1['text']
+    if s=='0':
+      s='' 
+    if num in "+-*/()":
+      reset=False
+    if reset==True and num in "0,1,2,3,4,5,6,7,8,9":
       s=""
       reset=False
     label1['text']=s+num
   except:
     label1['text']="ERROR"
-
 #主窗口
 root=Tk()
-root.wm_title("四王计算器2.0")
+root.wm_title("四王计算器4.0")
 #显示栏
 image_1=PhotoImage(file="display.gif")
-label1=Label(root,text="0",font = ('楷体',31),background="yellow",anchor="e")
+label1=Label(root,text="0",font = ('楷体',30),background="yellow",anchor="e")
 label2=Label(root,text="",font = ('楷体',20),anchor="e",compound='center',image=image_1)
-label1['width']=14
+label1['width']=15
 #label2['width']=21
 label1['height']=1
 # label2['height']=1
@@ -209,55 +222,17 @@ filemenu.add_separator()
 filemenu.add_command(label="退出", command=root.quit)
 #按钮设置
 img_o=PhotoImage(file="button.gif")
-showText=['sin','cos','tan','←']
-for i in range(4):
+showText=['sin','cos','tan']
+for i in range(3):
   b=Button(root,text=showText[i],compound='center',activebackground="blue",image=img_o,font = ('黑体',20),fg="blue")
   b.grid(row=3,column=i)
   b.bind("<Button-1>",buttonCallBack)
-showText="789/456*123-0.C+"
-for i in range(4):
+b=Button(root,text="Rad",compound='center',image=img_o,font = ('黑体',20),fg="blue")
+b.grid(row=3,column=3)
+showText="789/456*123-0←C+().="
+for i in range(5):
   for j in range(4):
     b=Button(root,text=showText[i*4+j],compound='center',image=img_o,font = ('黑体',20),fg="blue")
     b.grid(row=i+5,column=j)
     b.bind("<Button-1>",buttonCallBack)
-showText="()=^"
-for i in range(4):
-    b=Button(root,text=showText[i],compound='center',image=img_o,font = ('黑体',20),fg="blue")
-    b.grid(row=9,column=i)
-    b.bind("<Button-1>",buttonCallBack)
-# b=Button(root,text="=",compound='center',image=img_o,font = ('黑体',20),fg="blue")
-# b.grid(row=9,columnspan=2,sticky="we")
-# b.bind("<Button-1>",buttonCallBack)
 root.mainloop()
-
-
-
-# import math
-# import wx
- 
-# class CalcFrame(wx.Frame):
- 
-#     def __init__(self,title):
-#         super(CalcFrame, self).__init__(None, title = title, size = (300, 250))
-#         self.InitUI()
-#         self.Centre()
-#         self.Show()
- 
-#     def InitUI(self):
-#         vbox = wx.BoxSizer(wx.VERTICAL)#以列放置，文本框在最上方
-#         self.textprint = wx.TextCtrl(self, style=wx.TE_RIGHT)
- 
-#         vbox.Add(self.textprint, flag=wx.EXPAND|wx.TOP|wx.BOTTOM, border=4)      
-#         gridBox = wx.GridSizer(5, 4, 5, 5)##行数，列数，垂直间隔，水平间隔
-#         labels=['AC','DEL','PI','CLOSE','7','8','9','/','4','5','6','*','1','2','3','-','0','.','=','+']
-#         for label in labels: 
-#             gridBox.Add(wx.Button(self,label = label), 1, wx.EXPAND)
- 
-#         vbox.Add(gridBox, proportion=1, flag=wx.EXPAND)
-#         self.SetSizer(vbox)
- 
-# if __name__ == '__main__':
- 
-#     app = wx.App()
-#     CalcFrame(title = 'Calculator')
-#     app.MainLoop()
