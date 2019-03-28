49070e4a366fc9836d440120b92db44f79ef87e3
sad920787367 <48972262+sad920787367@users.noreply.github.com>
2019-03-28 13:51:15 +0800

    更新版本
    优化了显示问题，修复了运算符BUG

--- calculator.py
+++ calculator.py
@@ -169,66 +169,68 @@
 
 from tkinter import *
 from math import *
+#定义两个标志初始值设为True
 reset=True
+second=True
 def buttonCallBack(event):
   global label
   global reset
+  global second
   try:
-    num=event.widget['text']
-    if num=='C':
+    num=event.widget['text']  #num用来保存此次点击事件的文本
+    if num=='C':    #清屏设置
       label1['text']="0"
       label2['text']=''
       return
-    if num in "=":
+    if num in "=":   #等于号输出结果，使用eval函数进行计算，结果输出在label1上
       label2['text']=label1['text']
-      label1['text']=str(eval(label2['text']))[0:12]
+      label1['text']=str(eval(label1['text']))
       reset=True
+      second=True
       return
-    if num == "←":
+    if num == "←":     #退位键，用切片的方式舍掉label1的最后一位
       label1['text']=label1['text'][0:-1]
       return
-    s=label1['text']
+    s=label1['text']   #s是用来存储按下按钮前label1的内容
     if s=='0':
       s='' 
-    if num in "+-*/()":
+    if second==False and num in "+-*/":
+      num=""
+    if num in "'sin','cos','tan','sqrt'":  #三角函数和开平方使用后自动加上左括号
+      num=num+"("
       reset=False
-    if reset==True and num in "0,1,2,3,4,5,6,7,8,9":
+    if num in "0,1,2,3,4,5,6,7,8,9,0":
+      second=True
+    if second==True and num in "+-*/":
+      reset=False
+      second=False
+    if reset==True and num in "0,1,2,3,4,5,6,7,8,9,'sin','cos','tan','sqrt'":
       s=""
       reset=False
-    label1['text']=s+num
+    label1['text']=s+num   #将按下按钮前的label内容和此次点击的文本进行连接
   except:
     label1['text']="ERROR"
 #主窗口
 root=Tk()
 root.wm_title("四王计算器4.0")
+#用grid进行按钮和显示框的布局，row为行，column为列
+#在Label（）和Button（）中是按钮和显示框的基本属性，其上显示文本就是用text来操作的
 #显示栏
-image_1=PhotoImage(file="display.gif")
-label1=Label(root,text="0",font = ('楷体',30),background="yellow",anchor="e")
-label2=Label(root,text="",font = ('楷体',20),anchor="e",compound='center',image=image_1)
-label1['width']=15
-#label2['width']=21
-label1['height']=1
-# label2['height']=1
+image_1=PhotoImage(file="display.gif")  #打开图片，以备使用
+label1=Label(root,text="0",font = ('楷体',20),background="yellow",anchor="e",width=21,height=2)
+label2=Label(root,text="",font = ('楷体',15),anchor="e",compound='center',image=image_1)
 label1.grid(row=2,columnspan=4,sticky=W)
 label2.grid(row=1,columnspan=4,sticky=W)
-#菜单
-menu = Menu(root)
-root.config(menu=menu)#显示菜单
-filemenu = Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="Open")
-filemenu.add_command(label="Save")
-filemenu.add_separator()
-filemenu.add_command(label="退出", command=root.quit)
 #按钮设置
-img_o=PhotoImage(file="button.gif")
+img_o=PhotoImage(file="button.gif")  #打开图片，以备使用
 showText=['sin','cos','tan']
 for i in range(3):
   b=Button(root,text=showText[i],compound='center',activebackground="blue",image=img_o,font = ('黑体',20),fg="blue")
   b.grid(row=3,column=i)
   b.bind("<Button-1>",buttonCallBack)
-b=Button(root,text="Rad",compound='center',image=img_o,font = ('黑体',20),fg="blue")
+b=Button(root,text="sqrt",compound='center',image=img_o,font = ('黑体',18),fg="blue")
 b.grid(row=3,column=3)
+b.bind("<Button-1>",buttonCallBack)
 showText="789/456*123-0←C+().="
 for i in range(5):
   for j in range(4):
